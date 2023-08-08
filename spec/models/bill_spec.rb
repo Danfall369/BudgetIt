0bfb858742aab78310d719facc3cd5548cef1655
Daniel Falcon <Danfall963@Gmail.com>
2023-08-08 11:00:08 -0400

    Fix Rubocop issues

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Bill, type: :model do
-  describe "validations" do
-    it "is valid with valid attributes" do
-      author = User.create(name: "John Doe")
-      bill = Bill.new(name: "Electricity Bill", amount: 100.50, author: author)
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      author = User.create(name: 'John Doe')
+      bill = Bill.new(name: 'Electricity Bill', amount: 100.50, author:)
       expect(bill).to be_valid
     end
 
-    it "is invalid without a name" do
-      author = User.create(name: "John Doe")
-      bill = Bill.new(name: nil, amount: 100.50, author: author)
+    it 'is invalid without a name' do
+      author = User.create(name: 'John Doe')
+      bill = Bill.new(name: nil, amount: 100.50, author:)
       bill.valid?
       expect(bill.errors[:name]).to include("can't be blank")
     end
 
-    it "is invalid without an amount" do
-      author = User.create(name: "John Doe")
-      bill = Bill.new(name: "Electricity Bill", amount: nil, author: author)
+    it 'is invalid without an amount' do
+      author = User.create(name: 'John Doe')
+      bill = Bill.new(name: 'Electricity Bill', amount: nil, author:)
       bill.valid?
       expect(bill.errors[:amount]).to include("can't be blank")
     end
 
-    it "is invalid without an author" do
-      bill = Bill.new(name: "Electricity Bill", amount: 100.50, author: nil)
+    it 'is invalid without an author' do
+      bill = Bill.new(name: 'Electricity Bill', amount: 100.50, author: nil)
       bill.valid?
-      expect(bill.errors[:author]).to include("must exist")
+      expect(bill.errors[:author]).to include('must exist')
     end
   end
 
-  describe "associations" do
-    it "belongs to an author" do
+  describe 'associations' do
+    it 'belongs to an author' do
       association = described_class.reflect_on_association(:author)
       expect(association.macro).to eq :belongs_to
     end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  describe "validations" do
-    it "is valid with valid attributes" do
-      author = User.create(name: "John Doe")
-      service = Service.new(name: "Example Service", icon: "example.png", author: author)
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      author = User.create(name: 'John Doe')
+      service = Service.new(name: 'Example Service', icon: 'example.png', author:)
       expect(service).to be_valid
     end
 
-    it "is invalid without a name" do
-      author = User.create(name: "John Doe")
-      service = Service.new(name: nil, icon: "example.png", author: author)
+    it 'is invalid without a name' do
+      author = User.create(name: 'John Doe')
+      service = Service.new(name: nil, icon: 'example.png', author:)
       service.valid?
       expect(service.errors[:name]).to include("can't be blank")
     end
 
-    it "is invalid without an icon" do
-      author = User.create(name: "John Doe")
-      service = Service.new(name: "Example Service", icon: nil, author: author)
+    it 'is invalid without an icon' do
+      author = User.create(name: 'John Doe')
+      service = Service.new(name: 'Example Service', icon: nil, author:)
       service.valid?
       expect(service.errors[:icon]).to include("can't be blank")
     end
 
-    it "is invalid without an author" do
-      service = Service.new(name: "Example Service", icon: "example.png", author: nil)
+    it 'is invalid without an author' do
+      service = Service.new(name: 'Example Service', icon: 'example.png', author: nil)
       service.valid?
-      expect(service.errors[:author]).to include("must exist")
+      expect(service.errors[:author]).to include('must exist')
     end
   end
 
-  describe "associations" do
-    it "belongs to an author" do
+  describe 'associations' do
+    it 'belongs to an author' do
       association = described_class.reflect_on_association(:author)
       expect(association.macro).to eq :belongs_to
     end
